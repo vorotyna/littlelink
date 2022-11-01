798937cff7d53e687d2c27122e90a86e4af699e4
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-01 02:53:21 +0000

    Adding a GET and POST for new URL form

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); // tells the Express app to use EJS as its templating engine
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,12 +27,21 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with "Ok" (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   id = req.params.id;
   longUrl = urlDatabase[id]
   const templateVars = {id: id, longURL: longUrl };
   res.render("urls_show", templateVars);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
