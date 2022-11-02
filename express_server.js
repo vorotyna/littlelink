e9c782c9424cb505ee31ee22bd75b4a373afb560
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-02 05:18:40 +0000

    Displaying the username and creating a logout port

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); // tells the Express app to use EJS as its templating engine
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 function generateRandomString() {
   let shortString = "";
@@ -31,19 +32,29 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   id = req.params.id;
   longUrl = urlDatabase[id];
-  const templateVars = { id: id, longURL: longUrl };
+  const templateVars = {
+    id: id,
+    longURL: longUrl,
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -79,6 +90,12 @@ app.post("/urls/:id", (req, res) => {
 app.post("/login", (req, res) => {
   username = req.body.username;
   res.cookie('username', username);
+  res.redirect("/urls");
+});
+
+// Post request the logout route
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect("/urls");
 });
 
